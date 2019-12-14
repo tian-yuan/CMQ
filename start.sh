91e7d9eecbde6d45313b61aa4b13f41d96f32b91
liuzebo <hzliuzebo@corp.netease.com>
2019-12-14 14:02:24 +0800

    script: add start script

--- start.sh
+++ start.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 cd bin
-nohup ./hub mqtt >/dev/null 2>&1&
-nohup ./controller controller >/dev/null 2>&1&
-nohup ./message-dispatcher rpc >/dev/null 2>&1&
-nohup ./publish-engine rpc >/dev/null 2>&1&
-nohup ./registry rpc >/dev/null 2>&1&
-nohup ./topic-acl rpc >/dev/null 2>&1&
-nohup ./topic-manager rpc >/dev/null 2>&1&
+export IOT_HUB_CONFIG_LOG_APPNAME=iothub
+./hub mqtt >/dev/null 2>&1&
+export IOT_CONTROLLER_CONFIG_LOG_APPNAME=controller
+./controller controller >/dev/null 2>&1&
+export IOT_DISPATCHER_CONFIG_LOG_APPNAME=dispatcher
+./message-dispatcher rpc >/dev/null 2>&1&
+export IOT_ENGINE_CONFIG_LOG_APPNAME=engine
+./publish-engine rpc >/dev/null 2>&1&
+export IOT_REGISTRY_CONFIG_LOG_APPNAME=registry
+./registry rpc >/dev/null 2>&1&
+export IOT_TOPICACL_CONFIG_LOG_APPNAME=topicacl
+./topic-acl rpc >/dev/null 2>&1&
+export IOT_TOPICMANAGER_CONFIG_LOG_APPNAME=topicmanager
+./topic-manager rpc >/dev/null 2>&1&
